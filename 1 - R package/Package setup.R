6b66b431309385a5636ebc23c1de137d6307321d
Alyssa Bilinski <abilinski@Alyssas-MacBook-Pro.local>
2020-12-13 04:58:48 -0500

    R

---
+++ 1 - R package/Package setup.R
@@ -0,0 +1,25 @@
+#********************************** LOADING R PACKAGE *************************************#
+#                                                                                          #
+#                                                                                          #
+# This document implements a sample script to load and run the the BackToSchool R package. #
+#******************************************************************************************#
+
+# to update documentation
+.rs.restartR()
+library(roxygen2)
+library(devtools)
+wd = "~/Dropbox/Schools/Public code/1 - R package/"
+setwd(wd)
+
+# make 
+#create("BackToSchool")
+
+# make documentation
+setwd(paste0(wd, "BackToSchool"))
+document()
+build_manual()
+
+# to load
+setwd(wd)
+install("BackToSchool")
+
